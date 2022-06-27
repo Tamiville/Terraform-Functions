1c9264feea3c1de28f3ef42065fffcad0029d860
tamiville <tamiville01@outlook.com>
2022-06-27 20:53:20 +0300

    Added subnet standalone resource & route tables & attached nsg's

--- network/network.tf
+++ network/network.tf
@@ -16,21 +16,10 @@ resource "azurerm_virtual_network" "clax_vnet" {
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-   subnet {
-    name           = "database-subnet"
-    address_prefix = "10.0.1.0/24"
-    security_group = azurerm_network_security_group.clax_nsg.id
-  }
-
-  subnet {
-    name           = "application-subnet"
-    address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.clax_nsg.id
-  }
-
-   tags = {
+  
+  tags = {
     environment = "Development"
-    company = "elitesolutionsit"
+    company     = "elitesolutionsit"
     ManagedWith = "terraform"
   }
 }
@@ -40,5 +29,55 @@ resource "azurerm_route_table" "clax_rt" {
   resource_group_name           = azurerm_resource_group.clax_general_network.name
   disable_bgp_route_propagation = false
 }
- 
-    
+
+resource "azurerm_subnet" "database_subnet" {
+  name                 = "database_subnet"
+  resource_group_name  = azurerm_resource_group.clax_general_network.name
+  virtual_network_name = azurerm_virtual_network.clax_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
+}
+
+resource "azurerm_subnet" "application_subnet" {
+  name                 = "application_subnet"
+  resource_group_name  = azurerm_resource_group.clax_general_network.name
+  virtual_network_name = azurerm_virtual_network.clax_vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
+}
+
+resource "azurerm_subnet_route_table_association" "claxdev_rt_assoc_database" {
+  subnet_id      = azurerm_subnet.database_subnet.id
+  route_table_id = azurerm_route_table.clax_rt.id
+}
+
+resource "azurerm_subnet_route_table_association" "claxdev_rt_assoc_application" {
+  subnet_id      = azurerm_subnet.application_subnet.id
+  route_table_id = azurerm_route_table.clax_rt.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "claxdev_nsg_assoc_database_subnet" {
+  subnet_id                 = azurerm_subnet.database_subnet.id
+  network_security_group_id = azurerm_network_security_group.clax_nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "claxdev_nsg_assoc_application_subnet" {
+  subnet_id                 = azurerm_subnet.application_subnet.id
+  network_security_group_id = azurerm_network_security_group.clax_nsg.id
+}
